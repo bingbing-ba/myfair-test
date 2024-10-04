c316dfb3dc190b55ef88d46974112b92f48e0d53
binsan song <qlstks@gmail.com>
2024-10-04 16:12:14 +0900

    style input field

--- src/components/pages/TodoUserListPage.tsx
+++ src/components/pages/TodoUserListPage.tsx
@@ -1,6 +1,8 @@
 'use client';
+import React from 'react';
 import styled from '@emotion/styled';
 import TodoUserListHeader from '../parts/Header';
+import InputField from '../parts/InputField';
 
 const Container = styled.div`
   max-width: 760px;
@@ -19,6 +21,17 @@ const TodoUserListPage = ({}: Props) => {
   return (
     <Container>
       <TodoUserListHeader />
+      <InputField
+        style={{ marginTop: '64px' }}
+        placeholder="할일을 입력해 주세요"
+        onKeyDown={(e) => {
+          console.log(e.code);
+          if (e.code === 'Enter') {
+            // set todo
+            alert('set todo');
+          }
+        }}
+      />
     </Container>
   );
 };
---
+++ src/components/parts/InputField.tsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+const InputField = styled.input`
+  padding: 32px;
+  border-radius: 24px;
+  background: #e5e5e5;
+  color: #111;
+  border: none;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 28px;
+  text-align: left;
+  width: 100%;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default InputField;

02708183c4f9eb1e2ba65f1f68641fda253c2727
binsan song <qlstks@gmail.com>
2024-10-04 15:47:25 +0900

    style container and header

--- src/components/pages/TodoUserListPage.tsx
+++ src/components/pages/TodoUserListPage.tsx
@@ -1,15 +1,26 @@
-"use client";
-import React from "react";
-import styled from "@emotion/styled";
+'use client';
+import styled from '@emotion/styled';
+import TodoUserListHeader from '../parts/Header';
 
-const Container = styled.div``;
+const Container = styled.div`
+  max-width: 760px;
+  margin: 0 auto;
+  margin-top: 108px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 10px;
+`;
 
 interface Props {}
 
 const TodoUserListPage = ({}: Props) => {
-  return <Container>
-    todo
-  </Container>;
+  return (
+    <Container>
+      <TodoUserListHeader />
+    </Container>
+  );
 };
 
 export default TodoUserListPage;
---
+++ src/components/parts/Header.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Header = styled.div`
+  font-size: 56px;
+  font-weight: 700;
+  line-height: 72px;
+`;
+
+const TodoUserListHeader = () => {
+  return <Header>To Do List</Header>;
+};
+
+export default TodoUserListHeader;

ae3c2d1ac17a1dd42aae9ee6bc7ac2532761ba9f
binsan song <qlstks@gmail.com>
2024-10-07 20:30:06 +0900

    add test skeletons

--- src/components/pages/TodoUserListPage.spec.tsx
+++ src/components/pages/TodoUserListPage.spec.tsx
@@ -47,5 +47,15 @@ describe('Todo User List Page', () => {
 
       expect(todoLength).toBe(0);
     });
+
+    it('문자열을 입력하고 Enter 하면 todo가 생성된다.', async () => {});
+  });
+
+  describe('List', () => {
+    it('선택한 필터에 따라 해당 상태의 todo만 화면에 보인다', async () => {});
+
+    it('todo text나 체크박스를 클릭하면 todo의 완료 상태가 토글된다.', async () => {});
+
+    it('삭제 버튼을 누르면 해당 todo가 삭제된다. 화면에서도 보이지 않는다.', async () => {});
   });
 });
